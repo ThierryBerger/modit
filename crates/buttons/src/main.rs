139ab97ca82b1015ae52cf0ccf4fe25ebe922225
Thierry Berger <contact@thierryberger.com>
2025-06-29 14:50:07 +0200

    fix name

--- crates/buttons/src/main.rs
+++ crates/buttons/src/main.rs
@@ -79,7 +79,7 @@ fn main() -> ! {
                 create_advertising_data(&[
                     AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
                     AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
-                    AdStructure::CompleteLocalName(esp_hal::chip!()),
+                    AdStructure::CompleteLocalName(&format!("modit-{}", esp_hal::chip!())),
                 ])
                 .unwrap()
             )
